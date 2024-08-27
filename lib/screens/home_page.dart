c3a7894bc4f4005c1bffdb0857546340c6a518ca
tevesgithub <vjteves@gmail.com>
2024-08-27 09:20:58 -0600

    update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -11,6 +11,48 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   FirebaseFirestore db = FirebaseFirestore.instance;
 
+  final List<String> tasks = <String>[];
+  final List<bool> checkboxes = List.generate(8, (index) => false);
+  TextEditingController nameController = TextEditingController();
+
+  bool isChecked = false;
+
+  void addItemToList() async {
+    final String taskName = nameController.text;
+
+    await db.collection('tasks').add({
+      'name': taskName,
+      'completed': false,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
+
+    setState(() {
+      tasks.insert(0, taskName);
+    });
+  }
+
+  void removeItems(int index) async {
+    //Get the tasks to be removed
+    String taskToBeRemoved = tasks[index];
+
+    //Remove the task from Firestore
+    QuerySnapshot querySnapshot = await db
+        .collection('tasks')
+        .where('name', isEqualTo: taskToBeRemoved)
+        .get();
+
+    if (querySnapshot.size > 0) {
+      DocumentSnapshot documentSnapshot = querySnapshot.docs[0];
+
+      await documentSnapshot.reference.delete();
+    }
+
+    setState(() {
+      tasks.removeAt(index);
+      checkboxes.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
